6f2dfce48bee2cc2685c09e4293d910b7d1d97f3
pawel054 <pawel.marcisz1@gmail.com>
2024-02-02 21:18:32 +0100

    dqdqd

--- Quiz/Quiz/MainPage.xaml.cs
+++ Quiz/Quiz/MainPage.xaml.cs
@@ -12,7 +12,7 @@ namespace Quiz
     public partial class MainPage : ContentPage
     {
         private int currentQuestionIndex = 0;
-        private int currentScore = 0;        
+        private int currentScore = 0;
         private List<int> questions = new List<int>();
         private Stopwatch stopwatch = new Stopwatch();
         private List<double> times = new List<double>();
@@ -34,11 +34,21 @@ namespace Quiz
             currentScore = 0;
             currentQuestionIndex = 0;
             times.Clear();
+            startGameView.IsVisible = false;
+            finishGameView.IsVisible = false;
+            gameView.IsVisible = true;
             ShowNextQuestion();
+        }
+
+        private void FinishButton(object sender, EventArgs e)
+        {
+            finishGameView.IsVisible = false;
+            startGameView.IsVisible = true;
         }
 
         private void ShowNextQuestion()
         {
+            submitAnswerButton.IsEnabled = true;
             if (currentQuestionIndex < questions.Count)
             {
                 questionLabel.IsVisible = true;
@@ -46,6 +56,7 @@ namespace Quiz
                 answerEntry.IsVisible = true;
                 submitAnswerButton.IsVisible = true;
                 feedbackLabel.IsVisible = false;
+                feedbackFrame.IsVisible = false;
                 stopwatch.Restart();
             }
             else
@@ -56,24 +67,30 @@ namespace Quiz
 
         private async void SubmitAnswer(object sender, EventArgs e)
         {
+            submitAnswerButton.IsEnabled = false;
             stopwatch.Stop();
             int correctAnswer = questions[currentQuestionIndex] * 2;
             if (int.TryParse(answerEntry.Text, out int userAnswer) && userAnswer == correctAnswer)
             {
                 currentScore++;
                 feedbackLabel.Text = "Poprawna odpowiedź!";
+                feedbackFrame.BorderColor = Color.Green;
+                feedbackLabel.TextColor = Color.Green;
             }
             else
             {
                 feedbackLabel.Text = $"Niestety, to jest zła odpowiedź. Poprawna odpowiedź to: {correctAnswer}";
+                feedbackFrame.BorderColor = Color.Red;
+                feedbackLabel.TextColor = Color.Red;
             }
             feedbackLabel.IsVisible = true;
+            feedbackFrame.IsVisible = true;
 
             times.Add(stopwatch.Elapsed.TotalSeconds);
             currentQuestionIndex++;
             answerEntry.Text = string.Empty; // Clear answer entry
 
-            await Task.Delay(2000); // Short delay before next question
+            await Task.Delay(2500); // Short delay before next question
             ShowNextQuestion();
         }
 
@@ -86,11 +103,13 @@ namespace Quiz
 
         private void DisplayFinalResults(double totalTime)
         {
+            finishGameView.IsVisible = true;
+            gameView.IsVisible = false;
             questionLabel.IsVisible = false;
             answerEntry.IsVisible = false;
             submitAnswerButton.IsVisible = false;
-            feedbackLabel.Text = $"Koniec quizu. Twój wynik: {currentScore} punktów. Całkowity czas: {totalTime.ToString("F2")} sekund.";
-            feedbackLabel.IsVisible = true;
+            resultPoints.Text = currentScore.ToString();
+            resultTime.Text = totalTime.ToString("F2");
         }
 
         private void SaveResult(string userName, double totalTime, int score)
